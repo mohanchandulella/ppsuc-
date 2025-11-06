153c5bfc63a0b90dac7fc0e1688bea32bba8f90e
mohanchandulella <mohanchandulella@gmail.com>
2025-11-06 17:01:08 +0530

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main() 
+  {
+ int n,rev=0,rem,temp;
+ printf("enter number");
+ scanf("%d",&n);
+ temp=n;
+ while(n!=0)
+ {
+ 	rem=n%10;
+ }
+  if(rev==temp)	
+  {
+	printf("%d is palidrome",temp);   
+}
+else 
+{
+	printf("%d is not palindrome");
+}
+}
+		  	
+  	
+  
